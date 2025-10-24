eeb251f6a1fa6d4e3e8e146061a683fae4ddbd91
Eduardo Pinheiro Júnior <37462997+eduardopj@users.noreply.github.com>
2025-10-24 15:30:14 -0500

    aula dia 24-10-25

--- App.js
+++ App.js
@@ -2,11 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/home';
 import Profile from './src/screens/profile';
-import Testes from './src/screens/layouts';
 
 export default function App() {
   return (
-    <Testes />
+    <Home />
     
   );
 }
